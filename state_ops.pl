0586e2ce3f05a150f7ba8b845be61bd5f3138d23
Édouard Denommée <dene2303@usherbrooke.ca>
2019-09-07 20:51:48 -0400

    new starte operations file

---
+++ state_ops.pl
@@ -0,0 +1,16 @@
+move(1,[Id, Name, X, Y, B], [Id, Name, X, Yout, B]) :-
+    Yout is Y+1.
+move(2,[Id, Name, X, Y, B], [Id, Name, Xout, Y, B]) :-
+    Xout is X+1.
+move(3,[Id, Name, X, Y, B], [Id, Name, Xout, Y, B]) :-
+    Xout is X+1.
+move(4,[Id, Name, X, Y, B], [Id, Name, X, Yout, B]) :-
+    Yout is Y+1.
+move(5,[Id, Name, X, Y, B], [Id, Name, Xout, Y, B]) :-
+    Xout is X+1.
+move(6,[Id, Name, X, Y, B], [Id, Name, X, Yout, B]) :-
+    Yout is Y+1.
+move(7,[Id, Name, X, Y, B], [Id, Name, Xout, Y, B]) :-
+    Xout is X+1.
+move(8,[Id, Name, X, Y, B], [Id, Name, X, Yout, B]) :-
+    Yout is Y+1.
