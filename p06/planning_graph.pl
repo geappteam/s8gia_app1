88a6cf604bb8fc2471b10b962eb0ec62565bca1d
Anthony Parris <para2709@usherbrooke.ca>
2019-09-08 17:54:10 -0400

    refactoring pile to stack

--- p06/planning_graph.pl
+++ p06/planning_graph.pl
@@ -8,87 +8,92 @@ is_action(take(Direction)).
 is_action(none).
 
 % + InitialState : Current environment's state.
-% + Empty pile : Starts a pile of duos of any action and its benefit has
-% first element of the pile.
-pile_best_actions(InitState, []) :-
+% + [] : Empty BestActionsStack stack which starts a stack of duos of
+% any action and its benefit has first element of the stack.
+stack_best_actions(InitState, []) :-
     benefice(is_action(AnyAction), InitState, Benefice),
-    pile_best_actions(InitState, [[AnyAction|Benefice]|[]]).
+    stack_best_actions(InitState, [[AnyAction|Benefice]|[]]).
 
 % + InitialState : Current environment's state.
-% - BestActionsPile : Pile of all possible actions after adding.
+% - BestActionsStack : Stack of all possible actions after adding.
 % another best action at the top.
-% Note : Will add to the pile.
-pile_best_actions(InitState, BestActionsPile) :-
+% Note : Will add to the stack.
+stack_best_actions(InitState, BestActionsStack) :-
     benefice(is_action(BestAction), InitState, NewBenefice),
-    peek_stack(BestActionsPile,[[TopAction|TopBenefice]|_]),
+    peek_stack(BestActionsStack,[[TopAction|TopBenefice]|_]),
     TopBenefice < NewBenefice,
-    pile_best_actions(InitState, [[BestAction|NewBenefice]|BestActionsPile]).
+    stack_best_actions(InitState, [[BestAction|NewBenefice]|BestActionsStack]).
 
 % + InitialState : Current environment's state.
-% + BestActionsPile : Pile of all possible actions with the best action
-% at the top.
-% Note : Will not add to the pile.
-pile_best_actions(InitState, BestActionsPile) :-
+% + BestActionsStack : Stack of all possible actions with the best
+% action at the top. Note : Will not add to the stack.
+stack_best_actions(InitState, BestActionsStack) :-
     benefice(is_action(BestAction), InitState, NewBenefice),
-    peek_stack(BestActionsPile,[[TopAction|TopBenefice]|_]),
+    peek_stack(BestActionsStack,[[TopAction|TopBenefice]|_]),
     TopBenefice >= NewBenefice,
-    pile_best_actions(InitState, BestActionsPile).
+    stack_best_actions(InitState, BestActionsStack).
 
 % + InitialState : Current environment's state.
-% + BestActionsPile : Pile of all possible actions with the best action
-% at the top.
-% Note : Ensure to stop piling when all actions-of-all-directions'
-% benefits have been analyzed.
-pile_best_actions(InitState, BestActionsPile) :-
+% + BestActionsStack : Stack of all possible actions with the best
+% action at the top. Note : Ensure to stop stacking when all
+% actions-of-all-directions' benefits have been analyzed.
+stack_best_actions(InitState, BestActionsStack) :-
     benefice(is_action(BestAction), InitState, NewBenefice),
-    peek_stack(BestActionsPile,[[TopAction|TopBenefice]|_]),
+    peek_stack(BestActionsStack,[[TopAction|TopBenefice]|_]),
     TopBenefice >= NewBenefice,
 
-    member_stack([move(1)|_], BestActionsPile),
-    member_stack([move(2)|_], BestActionsPile),
-    member_stack([move(3)|_], BestActionsPile),
-    member_stack([move(4)|_], BestActionsPile),
-    member_stack([move(5)|_], BestActionsPile),
-    member_stack([move(6)|_], BestActionsPile),
-    member_stack([move(7)|_], BestActionsPile),
-    member_stack([move(8)|_], BestActionsPile),
+    member_stack([move(1)|_], BestActionsStack),
+    member_stack([move(2)|_], BestActionsStack),
+    member_stack([move(3)|_], BestActionsStack),
+    member_stack([move(4)|_], BestActionsStack),
+    member_stack([move(5)|_], BestActionsStack),
+    member_stack([move(6)|_], BestActionsStack),
+    member_stack([move(7)|_], BestActionsStack),
+    member_stack([move(8)|_], BestActionsStack),
 
-    member_stack([attack(1)|_], BestActionsPile),
-    member_stack([attack(2)|_], BestActionsPile),
-    member_stack([attack(3)|_], BestActionsPile),
-    member_stack([attack(4)|_], BestActionsPile),
-    member_stack([attack(5)|_], BestActionsPile),
-    member_stack([attack(6)|_], BestActionsPile),
-    member_stack([attack(7)|_], BestActionsPile),
-    member_stack([attack(8)|_], BestActionsPile),
+    member_stack([attack(1)|_], BestActionsStack),
+    member_stack([attack(2)|_], BestActionsStack),
+    member_stack([attack(3)|_], BestActionsStack),
+    member_stack([attack(4)|_], BestActionsStack),
+    member_stack([attack(5)|_], BestActionsStack),
+    member_stack([attack(6)|_], BestActionsStack),
+    member_stack([attack(7)|_], BestActionsStack),
+    member_stack([attack(8)|_], BestActionsStack),
 
-    member_stack([drop(1)|_], BestActionsPile),
-    member_stack([drop(2)|_], BestActionsPile),
-    member_stack([drop(3)|_], BestActionsPile),
-    member_stack([drop(4)|_], BestActionsPile),
-    member_stack([drop(5)|_], BestActionsPile),
-    member_stack([drop(6)|_], BestActionsPile),
-    member_stack([drop(7)|_], BestActionsPile),
-    member_stack([drop(8)|_], BestActionsPile),
+    member_stack([drop(1)|_], BestActionsStack),
+    member_stack([drop(2)|_], BestActionsStack),
+    member_stack([drop(3)|_], BestActionsStack),
+    member_stack([drop(4)|_], BestActionsStack),
+    member_stack([drop(5)|_], BestActionsStack),
+    member_stack([drop(6)|_], BestActionsStack),
+    member_stack([drop(7)|_], BestActionsStack),
+    member_stack([drop(8)|_], BestActionsStack),
 
-    member_stack([take(1)|_], BestActionsPile),
-    member_stack([take(2)|_], BestActionsPile),
-    member_stack([take(3)|_], BestActionsPile),
-    member_stack([take(4)|_], BestActionsPile),
-    member_stack([take(5)|_], BestActionsPile),
-    member_stack([take(6)|_], BestActionsPile),
-    member_stack([take(7)|_], BestActionsPile),
-    member_stack([take(8)|_], BestActionsPile),
+    member_stack([take(1)|_], BestActionsStack),
+    member_stack([take(2)|_], BestActionsStack),
+    member_stack([take(3)|_], BestActionsStack),
+    member_stack([take(4)|_], BestActionsStack),
+    member_stack([take(5)|_], BestActionsStack),
+    member_stack([take(6)|_], BestActionsStack),
+    member_stack([take(7)|_], BestActionsStack),
+    member_stack([take(8)|_], BestActionsStack),
 
-    member_stack([none|_], BestActionsPile).
+    member_stack([none|_], BestActionsStack).
+
+% + InitState : Initial state of the environment.
+% - ModifiedState : New state obtained after.
+% - [] : Empty ActionPlan queue of actions to do.
+%plan_graph(InitState, ModifiedState, []) :-
+
 
 % + InitState : Initial state of the environment
 % + Goal : To validate as last element of the ActionPlan queue
 % - ModifiedState : New state obtained after
 % - ActionPlan : Queue of actions to do
-%plan_graph(InitState, Goal, ModifiedState, ActionPlan) :-
+%plan_graph(InitState, ModifiedState, ActionPlan) :-
     % Solve which action has the best benefit
-    % TODO...
+    stack_best_actions(InitState, BestActionsStack),
+
 
     % The state of the environment has been changed
     % TODO...
@@ -98,3 +103,19 @@ pile_best_actions(InitState, BestActionsPile) :-
 
     % Recusivity
     % TODO...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
